15190a1413267a727312401f5388605895f6b214
Enoki <enokiun@gmail.com>
2022-11-08 17:06:02 +0300

    chore: formatted uwuki

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! A simple rust library for interacting with the Eludris API
-mod gateway;                                                  
-mod http;                                                     
-mod models;                                                   
+                                                  mod gateway;
+                                                     mod http;
+                                                   mod models;
                                                               
-pub use gateway::{Events, GatewayClient, GATEWAY_URL};        
-pub use http::{HttpClient, REST_URL};                         
+        pub use gateway::{Events, GatewayClient, GATEWAY_URL};
+                         pub use http::{HttpClient, REST_URL};
