459f26af137dccda3c0cfff9db5e5cd62b15b4a4
Enoki <enokiun@gmail.com>
2023-01-23 19:32:54 +0300

    feat: support state

--- src/command_handler.rs
+++ src/command_handler.rs
@@ -1,19 +1,19 @@
 use std::{collections::HashMap, fmt::Debug, sync::Arc};
 
-use eludrs::{todel::Message, HttpClient};
+use eludrs::todel::Message;
 use futures::future::BoxFuture;
 
 pub type CommandResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 
 #[derive(Clone)]
-pub struct Command<'a> {
+pub struct Command<'a, S: Debug + Send + Sync + Clone> {
     pub names: &'a [&'a str],
     pub description: &'a str,
     pub usage: &'a str,
-    pub func: fn(Arc<HttpClient>, Message, Option<String>) -> BoxFuture<'a, CommandResult>,
+    pub func: fn(Arc<S>, Message, Option<String>) -> BoxFuture<'a, CommandResult>,
 }
 
-impl<'a> Debug for Command<'a> {
+impl<'a, S: Debug + Send + Sync + Clone> Debug for Command<'a, S> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Command")
             .field("names", &self.names)
@@ -24,22 +24,24 @@ impl<'a> Debug for Command<'a> {
 }
 
 #[derive(Debug, Clone)]
-pub struct CommandRunner<'a> {
+pub struct CommandRunner<'a, S: Debug + Send + Sync + Clone> {
     prefix: String,
-    commands: Vec<Command<'a>>,
+    commands: Vec<Command<'a, S>>,
+    state: Arc<S>,
     lookup: HashMap<&'a str, usize>,
 }
 
-impl<'a> CommandRunner<'a> {
-    pub fn new(prefix: String) -> Self {
+impl<'a, S: Debug + Send + Sync + Clone> CommandRunner<'a, S> {
+    pub fn new(prefix: String, state: Arc<S>) -> Self {
         Self {
             prefix,
             commands: vec![],
+            state,
             lookup: HashMap::new(),
         }
     }
 
-    pub fn add_command(&mut self, command: Command<'a>) {
+    pub fn add_command(&mut self, command: Command<'a, S>) {
         for name in command.names {
             self.lookup.insert(name, self.commands.len());
         }
@@ -47,28 +49,33 @@ impl<'a> CommandRunner<'a> {
     }
 
     #[allow(dead_code)]
-    pub fn command(mut self, command: Command<'a>) -> Self {
+    pub fn command(mut self, command: Command<'a, S>) -> Self {
         self.add_command(command);
         self
     }
 
-    pub fn commands(mut self, commands: &[Command<'a>]) -> Self {
+    pub fn commands(mut self, commands: &[Command<'a, S>]) -> Self {
         for command in commands.iter().cloned() {
             self.add_command(command);
         }
         self
     }
 
-    pub fn get_commands(&self) -> &Vec<Command<'a>> {
+    pub fn state(mut self, state: Arc<S>) -> Self {
+        self.state = state;
+        self
+    }
+
+    pub fn get_commands(&self) -> &Vec<Command<'a, S>> {
         &self.commands
     }
 
-    pub fn get_command(&self, name: &str) -> Option<&Command<'a>> {
+    pub fn get_command(&self, name: &str) -> Option<&Command<'a, S>> {
         let index = self.lookup.get(name)?;
         self.commands.get(*index)
     }
 
-    pub async fn run_command(&self, client: Arc<HttpClient>, message: Message) -> CommandResult {
+    pub async fn run_command(&self, message: Message) -> CommandResult {
         if message.content.starts_with(&self.prefix) {
             let (command, args) = message.content[self.prefix.len()..]
                 .split_once([' ', '\n'])
@@ -76,7 +83,7 @@ impl<'a> CommandRunner<'a> {
                 .unwrap_or((message.content[self.prefix.len()..].trim(), None));
             if let Some(index) = self.lookup.get(command) {
                 if let Some(command) = self.commands.get(*index) {
-                    (command.func)(client, message, args).await?;
+                    (command.func)(Arc::clone(&self.state), message, args).await?;
                 }
             }
         }
